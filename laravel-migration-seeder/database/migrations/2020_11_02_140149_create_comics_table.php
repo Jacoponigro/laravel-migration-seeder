24efc990635141df6e72ed5df6eaeb3041d8dad4
Jacopo <jakonigro@gmail.com>
2020-11-02 15:27:12 +0100

    creato controller, errore powershell php artisan migrate

--- laravel-migration-seeder/database/migrations/2020_11_02_140149_create_comics_table.php
+++ laravel-migration-seeder/database/migrations/2020_11_02_140149_create_comics_table.php
@@ -15,6 +15,16 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string("title", 30);
+            $table->string("original_title", 40)->nullable();
+            $table->string("author", 40);
+            $table->string("edition",20);
+            $table->smallInteger("number");
+            $table->smallInteger("num_pages");
+            $table->year("release");
+            $table->float("price", 6,2);
+            $table->boolean("colored");
+            $table->string("cover")->default("https://via.placeholder.com/200x300");
             $table->timestamps();
         });
     }
